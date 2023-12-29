be4f1d8c66f2182446179745ba2ba1f172447078
JessahSeroje <jseroje.actionlabs@gmail.com>
2023-12-29 17:20:58 +0800

    initial commit

---
+++ js/example-01.js
@@ -0,0 +1,10 @@
+/* Example 1 ('Click' event & event.currentTarget) */
+
+const showCT = document.getElementById("showCT");
+// const showCT = document.querySelector('#showCT');
+
+showCT.addEventListener("click", (event) => {
+  console.log(event); //
+  console.log(event.type); // event type // in this case it's the click event
+  console.log(event.currentTarget); // event.currentTarget -> is the element on which the event handler is CURRENTLY executed/processed
+});
